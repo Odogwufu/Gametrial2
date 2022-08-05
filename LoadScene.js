17a3619f8e7589bc7704838a9b81bdb98fea9c32
Odogwufu <94500778+Odogwufu@users.noreply.github.com>
2022-08-05 19:58:21 -0400

    testing

--- LoadScene.js
+++ LoadScene.js
@@ -15,7 +15,7 @@ class LoadScene extends Phaser.Scene{
         this.load.image('bg-color', 'assets/images/bg_color.png');
         this.load.image('background', 'assets/images/background.png');
         this.load.image('red', 'assets/images/red2.png');
-        this.load.image('yellow', 'assets/images/yellow2.png');
+        this.load.image('yellow', 'assets/i mages/yellow2.png');
         
     }
     create(){
